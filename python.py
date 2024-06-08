28acb8486cb99bf0a5a87aa81e3f84383caef165
danny hategekimana <138707632+Dannypro1@users.noreply.github.com>
2024-06-08 14:55:33 +0200

    Create python.py

---
+++ python.py
@@ -0,0 +1,28 @@
+"""class person:
+    def function (self,i,h): 
+        return i + h
+p1=person()
+i=int(input("enter postive number = "))
+h=int(input("enter other postive number = "))
+c= p1.function(i,h)
+
+print(f"the is the sum {c}")"""
+
+import platform
+import datetime
+import math
+import json
+p=open("dy.text","r")
+print(p.read())
+x='{"danny":"pro","jonathan":"pro"}'
+f=json.loads(x)
+print(f)
+y=min(12,13,14)
+g=math.cos(180)
+print(y)
+print(g)
+#x=datetime.datetime.today()
+#print(x)
+#x=dir(platform)
+#print(x)
+
